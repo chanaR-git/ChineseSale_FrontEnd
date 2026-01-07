da42ec308c7e70e2a91fcc12fc67ece7a4f64ecb
chanaR-git <ck0548544421@gmail.com>
2026-01-07 21:12:58 +0200

    route for management & management wrapper

---
+++ src/app/gifts/components/add-gift/add-gift.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-gift',
+  imports: [],
+  templateUrl: './add-gift.html',
+  styleUrl: './add-gift.scss',
+})
+export class AddGift {
+
+}
