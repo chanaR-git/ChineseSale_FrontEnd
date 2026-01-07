b2a88839bc25382a74f464d6e28fc8b3c9c26cda
chanaR-git <ck0548544421@gmail.com>
2026-01-07 17:23:05 +0200

    register component

--- src/app/auth/components/register/register.ts
+++ src/app/auth/components/register/register.ts
@@ -5,12 +5,15 @@ import { MessageModule } from 'primeng/message';
 import { MessageService } from 'primeng/api';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ToastModule } from 'primeng/toast';
+import { PasswordModule } from 'primeng/password';
+import { InputMask } from 'primeng/inputmask';
+import { maxLength } from '@angular/forms/signals';
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.html',
     standalone: true,
-    imports: [ReactiveFormsModule, InputTextModule, ButtonModule, ToastModule, MessageModule],
+    imports: [ReactiveFormsModule, InputTextModule, ButtonModule, ToastModule, MessageModule,PasswordModule,InputMask],
     providers: [MessageService]
 })
 export class Register {
@@ -23,7 +26,9 @@ export class Register {
     registerForm = this.fb.group(
       {
         username: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]]
+        email: ['', [Validators.required, Validators.email]],
+        password: ['',[Validators.required,Validators.maxLength(15),Validators.minLength(7),Validators.pattern(".*[a-z].*"),Validators.pattern(".*[A-Z].*"),Validators.pattern(".*[0-9].*")]],
+        phone:['',Validators.required]
       });
 
     onSubmit() {
